59787f3bdad32af171ad459031b1ea75ec24641f
Johna R <Jones@usr110-171.perunanet.smu.edu>
2015-05-04 14:47:29 -0500

    Initial Commit

---
+++ travellingRogue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  travellingRogue
+//
+//  Created by Johna Rutz on 5/4/15.
+//  Copyright (c) 2015 Johna Rutz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ travellingRogue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  travellingRogue
+//
+//  Created by Johna Rutz on 5/4/15.
+//  Copyright (c) 2015 Johna Rutz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
